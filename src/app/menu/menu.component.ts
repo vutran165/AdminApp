3d8c044bf137f1682fba5722e97371210dc5747f
Trần Tuấn Vũ <vutran165@gmail.com>
2018-07-25 17:58:03 +0700

    1-25072018

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faTh, faServer, faChartBar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  faTh = faTh;
+  faServer = faServer;
+  faChartBar = faChartBar;
+
+  showMenu: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  addExpandClass(element: any){
+      if(element === this.showMenu)
+      {
+          this.showMenu = '0';
+      }else {
+        this.showMenu = element;
+      }
+  }
+
+  
 
 }
